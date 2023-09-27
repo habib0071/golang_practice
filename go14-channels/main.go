540be8e81ed97b63c56e0d6626c64b7ee3b6ebcc
Habib khan <mdhabibkhanshamrat2@gamil.com>
2023-09-27 17:46:22 +0800

    add folder

---
+++ go14-channels/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+// func main() {
+// 	str := printSomething("habib")
+// 	log.Println(str)
+// }
+
+// func printSomething(s string) string {
+// 	log.Println("ohona +", s)
+// 	return s + " + ohona"
+// }
+
+func main() {
+	intChan := make(chan int)
+
+}
+
+func CaluclatedValu()
